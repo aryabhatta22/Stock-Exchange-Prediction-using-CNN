58fbccac61d8b3a07905609e38922ea724391dff
aryabhatta <tarun.hive@hotmail.com>
2020-01-21 01:15:50 +0530

    CNN model fit into dataset. Intial accuracy 0.7125

--- script.py
+++ script.py
@@ -87,3 +87,48 @@ while i+Sample_size <= len(X_test[:,0]):
 
 
 # ---------------------- CNN ----------------------
+
+from keras.models import Sequential       
+from keras.layers import Convolution2D   
+from keras.layers import MaxPooling2D     
+from keras.layers import Flatten          
+from keras.layers import Dense            
+
+classifier = Sequential()
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+                         
+# ---------------------- Fitting the CNN ----------------------
+
+from keras.preprocessing.image import ImageDataGenerator
+                
+train_datagen = ImageDataGenerator(
+        rescale=1,
+        shear_range=0,
+        zoom_range=0,
+        horizontal_flip=False)
+
+test_datagen = ImageDataGenerator(rescale=1)
+
+training_set= train_datagen.flow_from_directory(
+        'Dataset/Training',
+        target_size=(64, 64),
+        batch_size=5,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'Dataset/Test',
+        target_size=(64, 64),
+        batch_size=5,
+        class_mode='binary')
+ 
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=197,
+        epochs=10,
+        validation_data=test_set,
+        validation_steps=49)
