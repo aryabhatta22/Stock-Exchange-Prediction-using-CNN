64039186bb18a68d2a87d2c30a8b35081aad87b0
aryabhatta <tarun.hive@hotmail.com>
2020-01-25 22:35:53 +0530

    Remark: model overfitting

--- script.py
+++ script.py
@@ -124,19 +124,19 @@ test_datagen = ImageDataGenerator(rescale=1)
 training_set= train_datagen.flow_from_directory(
         'Dataset/Training',
         target_size=(64, 64),
-        batch_size=5,
+        batch_size=32,
         class_mode='binary')
 
 
 test_set = test_datagen.flow_from_directory(
         'Dataset/Test',
         target_size=(64, 64),
-        batch_size=5,
+        batch_size=32,
         class_mode='binary')
  
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=197,
-        epochs=25,
+        steps_per_epoch=710,
+        epochs=32,
         validation_data=test_set,
-        validation_steps=49)
+        validation_steps=168)
