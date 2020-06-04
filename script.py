42a80afdfed4df2bbcff804e02eb59fd945add09
aryabhatta <tarun.hive@hotmail.com>
2020-06-04 21:23:16 +0530

    intial commit: model 2.0.0

--- script.py
+++ script.py
@@ -8,146 +8,80 @@ Created on Sat Jan 18 15:26:59 2020
 # ---------------------- Data preprocessing ----------------------
 
 import pandas as pd
-
-dataset = pd.read_csv('RELIANCE.NS.csv')
-X = dataset.iloc[:, 1: 5].values  
-
-X = X[X[:,0] != -1]
-Y =X[:, 3:4]-X[:, 0:1]
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0, shuffle = False )
-
-# ---------------------- Creating Synthetic images ----------------------
-import os
+import pandas_datareader.data as web
+import pandas_datareader as pdr
+import pandas as pd
 import numpy as np
+import datetime
+import os
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+start = datetime.datetime(2000, 1,1)
+end = datetime.datetime(2020, 1,1)
+df = web.DataReader('RELIANCE.NS', 'yahoo', start, end )
+
+# getting rid of Date column
+df = df.reset_index()
+df = df.drop(columns=['Date'])
+
+# check for null values
+df.isnull().any()
+
+# Normalizaing data
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler()
+prices = sc.fit_transform(df)
+
+# Matrix alottment
+
+profit = prices[:,3]- prices[:, 2]
+profit = profit.reshape(-1,1)
+
+
+# ---------------------- Creating Synthetic images & Dataset ----------------------
 
 my_path = os.getcwd()       # Path of current working directory
-Samplesize = [20,30,40,50,60,70,80,90]
+Samplesize = 20
 
                         # Creating trainig image set
 
 UpImageNo = 1
 DownImageNo = 1
 
-for Sample_size in Samplesize:
-    i=0
-    array = np.arange(Sample_size)
-    while (2*Sample_size)+i <= len(Y_train[:,0]):
-        plt.figure()
-        #Remove the visibility of axes
-        cur_axes = plt.gca()
-        cur_axes.axes.get_xaxis().set_visible(False)
-        cur_axes.axes.get_yaxis().set_visible(False)
-        fig1 = plt.gcf()
-        fig1.patch.set_visible(False)       #Remove the visibility of frame in the graph
-        print(i)
-        print(Sample_size)
-        if Y_train[i+Sample_size:(2*Sample_size)+i,0].sum() >=0:
-            my_file = 'UpGraph'+str(UpImageNo)+'.png'
-            plt.plot(array,X_train[i:i+Sample_size,0],array,X_train[i:i+Sample_size,3], color="black")
-            plt.fill_between(array, X_train[i:i+Sample_size,0], X_train[i:i+Sample_size,3], where=X_train[i:i+Sample_size,3] >= X_train[i:i+Sample_size,0], facecolor='green', interpolate=True)
-            plt.fill_between(array, X_train[i:i+Sample_size,0], X_train[i:i+Sample_size,3], where=X_train[i:i+Sample_size,3] <= X_train[i:i+Sample_size,0], facecolor='red', interpolate=True)
-            #plt.plot(array,X_train[i:i+Sample_size,3])
-            fig1.savefig(os.path.join(my_path,'Dataset/Training/Up/'+my_file), transparent= True)
-            UpImageNo = UpImageNo+1
-        else:
-            my_file = 'DownGraph'+str(DownImageNo)+'.png'
-            plt.plot(array,X_train[i:i+Sample_size,0],array,X_train[i:i+Sample_size,3], color="black")
-            plt.fill_between(array, X_train[i:i+Sample_size,0], X_train[i:i+Sample_size,3], where=X_train[i:i+Sample_size,3] >= X_train[i:i+Sample_size,0], facecolor='green', interpolate=True)
-            plt.fill_between(array, X_train[i:i+Sample_size,0], X_train[i:i+Sample_size,3], where=X_train[i:i+Sample_size,3] <= X_train[i:i+Sample_size,0], facecolor='red', interpolate=True)
-            #plt.plot(array,X_train[i:i+Sample_size,0])
-            #plt.plot(array,X_train[i:i+Sample_size,3])
-            fig1.savefig(os.path.join(my_path, 'Dataset/Training/Down/'+my_file), transparent= True)
-            DownImageNo = DownImageNo+1
-        i=i+Sample_size
 
-                        # Creating test image set
+i=0
+X = np.empty((0,20,20), dtype = float)
+Y = np.empty((0,), dtype = int)
 
-UpImageNo = 1
-DownImageNo = 1
+while((i+Samplesize) < len(prices)):
+    CorrMatrix = np.corrcoef(prices[i: i+Samplesize][:])
+    X = np.insert(X, i, CorrMatrix.reshape(1,20,20), axis = 0)
+    if(profit[i:i+Samplesize][0].sum(axis = 0) > 0):
+        Y = np.insert(Y, i, 1, axis = 0)
+    else:
+        Y = np.insert(Y, i, 0, axis = 0)
+        
+                    # Saving heatmap
+    plt.figure()
+    #Remove the visibility of axes
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_visible(False)
+    cur_axes.axes.get_yaxis().set_visible(False)
+    fig1 = plt.gcf()
+    fig1.patch.set_visible(False)       #Remove the visibility of frame in the graph
+    if(Y[i] == 1):
+        my_file = 'UpGraph'+str(UpImageNo)+'.png'
+        sns.heatmap(CorrMatrix, cmap='coolwarm', cbar =False)
+        #plt.plot(array,X_train[i:i+Sample_size,3])
+        fig1.savefig(os.path.join(my_path,'Dataset/Up/'+my_file), transparent= True)
+        UpImageNo = UpImageNo+1
+    else:
+        my_file = 'DownGraph'+str(DownImageNo)+'.png'
+        sns.heatmap(CorrMatrix, cmap='coolwarm', cbar =False)
+        #plt.plot(array,X_train[i:i+Sample_size,3])
+        fig1.savefig(os.path.join(my_path, 'Dataset/Down/'+my_file), transparent= True)
+        DownImageNo = DownImageNo+1
+    i+=1
 
-for Sample_size in Samplesize:
-    array = np.arange(Sample_size)
-    i=0
-    while (2*Sample_size)+i <= len(Y_test[:,0]):
-        plt.figure()
-        #Remove the visibility of axes
-        cur_axes = plt.gca()
-        cur_axes.axes.get_xaxis().set_visible(False)
-        cur_axes.axes.get_yaxis().set_visible(False)
-        fig1 = plt.gcf()
-        fig1.patch.set_visible(False)       #Remove the visibility of frame in the graph
-        print(i)
-        print(Sample_size)
-        if Y_test[i+Sample_size:(2*Sample_size)+i,0].sum() >=0:
-            my_file = 'UpGraph'+str(UpImageNo)+'.png'
-            plt.plot(array,X_test[i:i+Sample_size,0],array,X_test[i:i+Sample_size,3], color="black")
-            plt.fill_between(array, X_test[i:i+Sample_size,0], X_test[i:i+Sample_size,3], where=X_test[i:i+Sample_size,3] >= X_test[i:i+Sample_size,0], facecolor='green', interpolate=True)
-            plt.fill_between(array, X_test[i:i+Sample_size,0], X_test[i:i+Sample_size,3], where=X_test[i:i+Sample_size,3] <= X_test[i:i+Sample_size,0], facecolor='red', interpolate=True)
-            fig1.savefig(os.path.join(my_path,'Dataset/Test/Up/'+my_file), transparent= True)
-            UpImageNo = UpImageNo+1
-        else:
-            my_file = 'DownGraph'+str(DownImageNo)+'.png'
-            plt.plot(array,X_test[i:i+Sample_size,0],array,X_test[i:i+Sample_size,3], color="black")
-            plt.fill_between(array, X_test[i:i+Sample_size,0], X_test[i:i+Sample_size,3], where=X_test[i:i+Sample_size,3] >= X_test[i:i+Sample_size,0], facecolor='green', interpolate=True)
-            plt.fill_between(array, X_test[i:i+Sample_size,0], X_test[i:i+Sample_size,3], where=X_test[i:i+Sample_size,3] <= X_test[i:i+Sample_size,0], facecolor='red', interpolate=True)
-            fig1.savefig(os.path.join(my_path, 'Dataset/Test/Down/'+my_file), transparent= True)
-            DownImageNo = DownImageNo+1
-        i=i+Sample_size
-
-
-# ---------------------- CNN ----------------------
-
-from keras.models import Sequential       
-from keras.layers import Convolution2D   
-from keras.layers import MaxPooling2D     
-from keras.layers import Flatten          
-from keras.layers import Dense            
-from keras.layers import Dropout
-
-
-classifier = Sequential()
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Dropout(0.5))
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Dropout(0.5))
-classifier.add(Flatten())
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-                         
-# ---------------------- Fitting the CNN ----------------------
-
-from keras.preprocessing.image import ImageDataGenerator
-                
-train_datagen = ImageDataGenerator(
-        rescale=1,
-        shear_range=0,
-        zoom_range=0,
-        horizontal_flip=False)
-
-test_datagen = ImageDataGenerator(rescale=1)
-
-training_set= train_datagen.flow_from_directory(
-        'Dataset/Training',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='binary')
-
-
-test_set = test_datagen.flow_from_directory(
-        'Dataset/Test',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='binary')
- 
-classifier.fit_generator(
-        training_set,
-        steps_per_epoch=710,
-        epochs=32,
-        validation_data=test_set,
-        validation_steps=168)
